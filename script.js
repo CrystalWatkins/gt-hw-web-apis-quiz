87d76a1ef4d3ea09e157fa8823ffce2e435b283e
Crystal <crystalwatkins428@gmail.com>
2020-06-10 22:28:51 -0400

    updated files

--- script.js
+++ script.js
@@ -1,66 +1,111 @@
-//View Highscores in top left corner
-// Time with timer countdown in top right corner
-//Welcome page with start quiz button
-//has script that reads: coding quiz challenge as H1 p= Try to answer the following code-related
-//questions within the time limit. Keep in mind that incorrect answers will penalize your score/ 
-// time by ten seconds with start quiz button at the bottom
-// start with 75 seconds
-// 01- Commonly Used data types DO NOT Include: 
-//1. strings 2. booleans 3. alerts** 4. numbers
-// 02-The condition in an if/ else statement is enclosed within ___. 
-//1. quotes 2. curly brackets 3.parenthesis 4. square brackets
-//03- Arrays in JavaScript can be used to store __.
-//1. numbers and strings 2. other arrays 3. booleans** 4. all of the above
-//04-Sting values must be enclosed within __ when being assigned to variables.
-// 1. commas 2. curly brackets 3. quotes 4. parentheses
-//05-A very useful tool used during development and debugging for printing content to the debugger is:
-// 1. JavaScript 2. terminal/ bash 3. for loops 4. console.log
-//after each click- put correct or wrong at the bottom
-//if wrong- subtract 15 seconds from timer/ score
-//at end of 5 questions- it will say "all done// your final score is _(time left)" Enter initials
-//when submit button is hit for initials- it will show hight scores.
-// underneath are two buttons to go back and clear high scores
-// console.log("hello world")
 
-// var startTimer = confirm("Would you like to start the timer?");
-var secondsLeft= 75;
+var secondsLeft = 75;
 var timeEl = document.querySelector("#timer");
-var startGame = document.querySelector("#startGame");
+var startGame = document.getElementById("startGame");
 var startUpPage = document.querySelector("#startUpPage");
-var firstQuestionPage = document.querySelector("#firstQuestionPage");
-var firstButton = document.querySelector("#firstButton");
-var secondQuestionPage = document.querySelector("#secondQuestionPage");
-var secondButton = document.querySelector("#secondButton");
-var thirdQuestionPage = document.querySelector("#thirdQuestionPage");
-var thirdButton = document.querySelector("#thirdButton");
-var fourthQuestionPage = document.querySelector("#fourthQuestionPage");
-var fourthButton = document.querySelector("#fourthButton");
-var fifthQuestionPage = document.querySelector("#fifthQuestionPage");
-var fifthButton = document.querySelector("#fifthButton");
+var questionPages = document.querySelector("#questionPages");
+var questionHeader = document.querySelector("#questionHeader");
+var selectionA = document.querySelector("#selectionA");
+var selectionB = document.querySelector("#selectionB");
+var selectionC = document.querySelector("#selectionC");
+var selectionD = document.querySelector("#selectionD");
+var endOfQuizPage = document.querySelector("#endOfQuizPage")
 
-function setTime () {
-    startGame.addEventListener("click", function setTime() {
-    startUpPage.style.display= "none"
-    firstQuestionPage.style.display = "block"
-    var timerInterval = setInterval(function() {
-    secondsLeft-- ;
+startGame.addEventListener("click", function () {
+  startUpPage.style.display = "none"
+  questionPages.style.display = "block"
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
     timeEl.textContent = secondsLeft;
 
-    if(secondsLeft === 0) {
+    if (secondsLeft === 0) {
       clearInterval(timerInterval);
     }
   }, 1000);
 });
+
+var wrongAnswer = function() {
+  console.log ("wrong answer")
+  .addEventListener("click", function () {
+    currentQuestionIndex++;
+  });
+} 
+var rightAnswer = function () {
+  console.log("right answer")
+  selection.addEventListener("click", function () {
+    currentQuestionIndex++;
+  });
 }
 
-firstButton.addEventListener("click",function() {
-    firstQuestionPage.style.display = "none";
-    secondQuestionPage.style.display= "block";
-});
+var arrayOfQuestionPages = [
+  {
+    question: "Commonly Used data types DO NOT Include:",
+    selections: ["<button onclick = 'wrongAnswer()'>strings</button>", "<button onclick = 'wrongAnswer()'>booleans</button>", "<button onclick = 'rightAnswer()'>alerts</button>", "<button onclick = 'wrongAnswer()'>numbers</button>"],
+    answer: "alerts",
+  },
+  {
+    question: "The condition in an if/ else statement is enclosed within ___.",
+    selections: ["<button onclick = 'wrongAnswer()'>quotes</button>", "<button onclick = 'wrongAnswer()'>curly brackets</button>", "<button onclick = 'rightAnswer()'>parenthesis</button>", "<button onclick = 'wrongAnswer()'>square brackets</button>"],
+    answer: "parenthesis",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store __.",
+    selections: ["<button onclick = 'wrongAnswer()'>numbers and strings</button>", "<button onclick = 'wrongAnswer ()'>other arrays</button>", "<button onclick = 'wrongAnswer'>booleans</button>","<button onclick = 'rightAnswer'> all of the above</button>"],
+    answer: "all of the above",
+  },
+  {
+    question: "Sting values must be enclosed within __ when being assigned to variables.",
+    selections: [ "<button onclick = 'wrongAnswer()'>commas</button>", "<button onclick = 'wrongAnswer()'>curly brackets</button>","<button onclick= 'rightAnswer'>quotes</button>", "<button onclick = 'wrongAnswer'>parenthesis</button>"],
+    answer: "quotes",
+  },
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    selections: ["<button onclick = 'wrongAnswer()'>Javascript</button>", "<button onclick = 'wrongAnswer()'>terminal bash</button>", "<button onclick = 'wrongAnswer()'>for loops</button>", "<button onclick = 'rightAnswer'()>console.log</button>"],
+    answer: "console.log",
+  },
+]
 
-setTime();
+var currentQuestionIndex = 0;
+
+function generateQuestion() {
+  if (questionIndex > 4 || timer > 0) {
+    endOfQuizPage.style.display = "block";
+  }
+  else {
+    // for (let i = 0; i < arrayOfQuestionPages.length; i++){
+    //   addEventListener "click"
+    // }
+    console.log('made it to here!')
+    var currentQuestion = arrayOfQuestionPages[currentQuestionIndex];
+    questionPages.innerHTML = "";
+    var question = document.querySelector("questionPages");
+    question.textContent = currentQuestion.selections;
+  }
+}
+
+for(let i = 0; i < arrayOfQuestionPages.length; i++){
+  var question = arrayOfQuestionPages[i].question;
+  var selections = arrayOfQuestionPages[i].selections;
+  var answer = arrayOfQuestionPages[i].answer;
+  if (i === 0) {
+    console.log(question);
+    console.log(selections);
+    console.log(answer);
+    console.log(questionHeader);
+    questionHeader.textContent = `Question: ${question}`;
+    for (let j = 0; j < selections.length; j++) {
+      if (j === 0) {
+          selectionA.innerHTML = selections[0]
+      }else if (j === 1) {
+          selectionB.innerHTML = selections[1]
+      } else if (j === 2) {
+          selectionC.innerHTML = selections[2]
+      } else if (j === 3) {
+          selectionD.innerHTML = selections[3]
+      }
+      }
+    }
+}
+ 
 
 
-
-
-// playButton.addEventListener("click", countdownTimer);
